19d71d6ac89a9b3c412021f2ff056ca691ab050f
shumailakayani06 <shumailakayani059@gmail.com>
2023-02-03 10:41:09 +0100

    added port

--- server.js
+++ server.js
@@ -1,25 +1,83 @@
 const express = require('express');
 const cors = require('cors');
-const { getTweets } = require('./services/database');
-
 const app = express();
-const PORT = 3000;
+const jwt = require('jsonwebtoken');
+const { getTweets, getTweetsByUsername, createTweet, getUserByUserName } = require('./services/database');
+
+const PORT = process.env.PORT  || 3333;
+const APP_SECRET = 'my-secret-key-1234';
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('hello from twitter api')
+app.get('/', (req, res) => {
+  res.send('Hello from Twitter API');
+});
+
+app.get('/tweets', async (req, res) => {
+  const tweets = await getTweets();
+  res.json(tweets);
+});
+
+app.get('/tweets/:username', async (req, res) => {
+  const { username } = req.params;
+  const tweets = await getTweetsByUsername(username);
+  res.json(tweets);
+});
+
+app.post('/tweets', async (req, res) => {
+  const { text } = req.body;
+  const token = req.headers['x-user'];
+
+ try{
+    const payload = jwt.verify(token, Buffer.from(APP_SECRET, 'base64'));
+    const username = payload.username;
+    const newTweet = await createTweet(username, text);
+    res.json(newTweet);
+ }catch(error){
+    res.status(401).send({error: 'Unable to verify token - not able to tweet'})
+ }
+});
+
+app.post('/login', async(req, res) =>{
+    const {username, password} = req.body;
+     
+   try{
+    const user = await getUserByUserName(username);
+
+    if(!user){
+        res.status(401).send({error: 'Unknown user - not found'});
+        return;
+    }
+
+    if(password !== user.password){
+        res.status(401).send({error: 'Wrong password'});
+    }
+
+    const token = jwt.sign({
+
+        id: user.id,
+        username : user.username,
+        name: user.name
+    }, Buffer.from(APP_SECRET, 'base64'));
+
+    res.json({token});
+   }catch (error){
+    res.status(500).send({error : error.message})
+   }
 })
 
-app.get('/tweets', async(req,res) =>{
-    //get tweets from data base 
-    //respond with tweets as JSON
- const tweets = await getTweets();
- res.json(tweets);
-
-});
+app.get('/session', async (req, res) =>{
+    const token = req.header['x-token'];
+   try{
+    const payload = jwt.verify(token, Buffer.from(APP_SECRET, 'base64'));
+    res.json({message: `You are logged in as ${payload.username}`})
+   }catch (error){
+         res.status(401).send({error: 'Invalid token'});
+   }
+    
+} )
 
 app.listen(PORT, () => {
-    console.log(`Twitter api listening to port: ${PORT}`)
-});
+  console.log(`Twitter API listening to port: ${PORT}`);
+})
--- services/database.js
+++ services/database.js
@@ -1,31 +1,86 @@
 const { Pool } = require('pg');
 
 const database = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'twitterDB',
-    password: 'Duakiyani1',
-    port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'twitterDB',
+  password: 'Duakiyani1',
+  port: 5432,
 });
 
-async function getTweets(){
-    const result = await database.query(`
-      SELECT
-       tweets.id,
-       tweets.message,
-       tweets.created_at,
-       users.name,
-       users.username
-      FROM
-        tweets
-      INNER JOIN users ON
-        tweets.user_id = users.id
-      ORDER BY created_at DESC;    
-    `);
-    console.log(result);
-    return result.rows;
+async function getTweets() {
+  const result = await database.query(`
+    SELECT
+      tweets.id,
+      tweets.message,
+      tweets.created_at,
+      users.name,
+      users.username
+    FROM
+      tweets
+    INNER JOIN users ON
+      tweets.user_id = users.id
+    ORDER BY created_at DESC;
+  `);
+  //console.log(result);
+  return result.rows;
+}
+
+async function getTweetsByUsername(username) {
+  const result = await database.query(`
+    SELECT
+      tweets.id,
+      tweets.message,
+      tweets.created_at,
+      users.name,
+      users.username
+    FROM
+      tweets
+    INNER JOIN users ON
+      tweets.user_id = users.id
+    WHERE
+      users.username = $1
+    ORDER BY created_at DESC;
+  `, [username]);
+
+  return result.rows;
+}
+
+async function createTweet(username, text) {
+  const userResult = await database.query(`
+    SELECT
+      users.id
+    FROM 
+      users
+    WHERE
+      users.username = $1
+  `, [username]);
+  const user = userResult.rows[0];
+
+  const tweetResult = await database.query(`
+    INSERT INTO tweets
+      (message, user_id)
+    VALUES
+      ($1, $2)
+    RETURNING
+      id
+  `, [text, user.id]);
+  const newTweet = tweetResult.rows[0];
+  return newTweet;
+}
+
+async function getUserByUserName(username){
+  const result = await database.query(`
+  SELECT * FROM users
+  WHERE
+  username = $1
+  `, [username]);
+  return result.rows[0];
 }
 
 module.exports = {
-getTweets,
-}
+  getTweets,
+  getTweetsByUsername,
+  createTweet,
+  getUserByUserName,
+};
